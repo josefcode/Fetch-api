9e918b3d8337bdf0499626da11c8504ed7c5f5c3
josefcode <atef.chela@gmail.com>
2022-03-31 13:41:22 -0300

    fetch api training

--- js/index.js
+++ js/index.js
@@ -6,17 +6,53 @@ fetch('https://randomuser.me/api/')
     .then(data => {
         //manipulamos a resposta
         console.log(data)
+        return renderizarDadosUsuario(data)
+    })
+    .catch((err) => {
+        console.log(err);
     });
 
+     
 function renderizarDadosUsuario(dados) {
-    /* -------------------------------- Tarefa 1 -------------------------------- */
-    // Aqui devem desenvolver uma função que seja exibida na tela:
-    // a foto, o nome completo do usuário e o e-mail.
-    // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
+
+    // submit our first object to variabel
+    let results = dados.results
+
+    results.map(item=>{
+     // Select element
+     console.log(item)
+    let card = document.querySelector('.card')
+    // create Elements
+    let ul = document.createElement('ul')
+    let li = document.createElement('li')
+    let img = document.createElement('img')
+    let title = document.createElement('h2')
+    let email = document.createElement('p')
+    let button = document.createElement('button')
+
+    // link our children to parent element
+    card.appendChild(ul)
+    ul.appendChild(li)
+    li.appendChild(img)
+    li.appendChild(title)
+    li.appendChild(email)
+    li.appendChild(button)
+    // add data to our elements
+    button.innerHTML = "Pedir random"
+    img.src = item.picture.medium;
+    title.innerHTML =`${item.name.title}. ${item.name.first} ${item.name.last}`
+    email.innerHTML = `${item.email}`
+
+    // add style to our emelents
+    ul.classList.add('list')
+
+    // function to change the data
+    button.onclick = ()=>{
+        document.location.reload(true)
+    }
+    })
+ 
+    console.log(dados)
 }
 
 
-/* --------------------------- Tarefa 2 (extra) --------------------------- */
-// Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
-// Você pode descomentar o código no index.html e usar esse botão para executar uma nova solicitação API, sem recarregar a página.
-// Cabe aos desenvolvedores decidirem qual bloco de código deve ser contido dentro de uma função para que ele possa ser executado toda vez que um clique de botão for realizado.
